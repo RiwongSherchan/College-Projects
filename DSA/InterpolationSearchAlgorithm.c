efe9cab3848ee5a181e007d061d9feeb704e9979
RiwongSherchan <sherchanriwong@gmail.com>
2023-06-05 18:09:26 +0545

    updated Interpolation seach and Linear search updates with 3 cases for 3 input sizes each

--- DSA/InterpolationSearchAlgorithm.c
+++ DSA/InterpolationSearchAlgorithm.c
@@ -42,7 +42,7 @@ int interpolation_search(int arr[], int size, int target) {
     return -1;
 }
 
-double measureElapsedTime(int arr[], int size, int target) {
+long long measureElapsedTime(int arr[], int size, int target) {
     LARGE_INTEGER frequency, start, end;
     QueryPerformanceFrequency(&frequency);
     QueryPerformanceCounter(&start);
@@ -52,67 +52,146 @@ double measureElapsedTime(int arr[], int size, int target) {
     interpolation_search(arr, size, target);
 
     QueryPerformanceCounter(&end);
-    double elapsed_time = (double)(end.QuadPart - start.QuadPart) / frequency.QuadPart;
+    long long elapsed_time = (end.QuadPart - start.QuadPart) * 1000000000 / frequency.QuadPart;
 
     return elapsed_time;
 }
 
-SIZE_T measureMemoryUsage(int arr[], int size, int target) {
-    HANDLE process = GetCurrentProcess();
-    PROCESS_MEMORY_COUNTERS_EX mem_info;
-    GetProcessMemoryInfo(process, (PROCESS_MEMORY_COUNTERS*)&mem_info, sizeof(mem_info));
-
-    interpolation_search(arr, size, target);
-
-    return mem_info.PeakWorkingSetSize;
+void printMemoryUsage(PROCESS_MEMORY_COUNTERS_EX pmc) {
+    SIZE_T virtualMemoryUsed = pmc.PrivateUsage;
+    SIZE_T peakWorkingSetSize = pmc.PeakWorkingSetSize;
+    printf("Memory Usage: %llu bytes\n", (unsigned long long)virtualMemoryUsed);
+    printf("Peak Working Set Size: %llu bytes\n", (unsigned long long)peakWorkingSetSize);
 }
 
+
 int main() {
-    int size;
-    printf("Enter the size of the array: ");
-    scanf("%d", &size);
-
-    int* arr = (int*)malloc(size * sizeof(int));
-    if (arr == NULL) {
-        printf("Memory allocation failed. Exiting...\n");
-        return 1;
-    }
-
     srand(time(NULL));  // Seed the random number generator
 
-    printf("Generated array: ");
-    for (int i = 0; i < size; ++i) {
-        arr[i] = rand() % 100;  // Generate random numbers between 0 and 99
-        printf("%d ", arr[i]);
+    // Define different array sizes for each case
+    int sizes[] = {5000, 10000, 15000};
+
+    for (int i = 0; i < sizeof(sizes) / sizeof(sizes[0]); ++i) {
+        int size = sizes[i]; // Size of the array
+
+        printf("// Case %d (Size: %d)\n", i + 1, size);
+
+        // Best Case: Already Sorted Array
+        printf("Best Case Started\n");
+
+        int* best_arr = (int*)malloc(size * sizeof(int));
+        if (best_arr == NULL) {
+            printf("Memory allocation failed. Exiting...\n");
+            return 1;
+        }
+
+        for (int j = 0; j < size; ++j) {
+            best_arr[j] = j; // Assigning numbers in sorted order
+        }
+
+        int best_target = size - 1; // Target element is the last element
+
+        long long best_elapsed_time = measureElapsedTime(best_arr, size, best_target);
+        PROCESS_MEMORY_COUNTERS_EX best_pmc;
+        GetProcessMemoryInfo(GetCurrentProcess(), (PROCESS_MEMORY_COUNTERS*)&best_pmc, sizeof(best_pmc));
+
+        insertion_sort(best_arr, size); // Sort the array
+
+        GetProcessMemoryInfo(GetCurrentProcess(), (PROCESS_MEMORY_COUNTERS*)&best_pmc, sizeof(best_pmc));
+        SIZE_T best_memory_usage = best_pmc.PrivateUsage;
+
+        int best_index = interpolation_search(best_arr, size, best_target);
+
+        if (best_index != -1)
+            printf("Element found at index %d\n", best_index);
+        else
+            printf("Element not found\n");
+
+        printf("Time elapsed: %lld nanoseconds\n", best_elapsed_time);
+        printMemoryUsage(best_pmc);
+
+        free(best_arr);
+
+        printf("Best Case Ended\n\n");
+
+        // Average Case: Randomized Array
+        printf("Average Case Started\n");
+
+        int* average_arr = (int*)malloc(size * sizeof(int));
+        if (average_arr == NULL) {
+            printf("Memory allocation failed. Exiting...\n");
+            return 1;
+        }
+
+        for (int j = 0; j < size; ++j) {
+            average_arr[j] = rand() % 1001; // Generate random numbers between 0 and 1000
+        }
+
+        int average_target = rand() % 1001; // Random target element
+
+        long long average_elapsed_time = measureElapsedTime(average_arr, size, average_target);
+        PROCESS_MEMORY_COUNTERS_EX average_pmc;
+        GetProcessMemoryInfo(GetCurrentProcess(), (PROCESS_MEMORY_COUNTERS*)&average_pmc, sizeof(average_pmc));
+
+        insertion_sort(average_arr, size); // Sort the array
+
+        GetProcessMemoryInfo(GetCurrentProcess(), (PROCESS_MEMORY_COUNTERS*)&average_pmc, sizeof(average_pmc));
+        SIZE_T average_memory_usage = average_pmc.PrivateUsage;
+
+        int average_index = interpolation_search(average_arr, size, average_target);
+
+        if (average_index != -1)
+            printf("Element found at index %d\n", average_index);
+        else
+            printf("Element not found\n");
+
+        printf("Time elapsed: %lld nanoseconds\n", average_elapsed_time);
+        printMemoryUsage(average_pmc);
+
+        free(average_arr);
+
+        printf("Average Case Ended\n\n");
+
+        // Worst Case: Descending Order Array
+        printf("Worst Case Started\n");
+
+        int* worst_arr = (int*)malloc(size * sizeof(int));
+        if (worst_arr == NULL) {
+            printf("Memory allocation failed. Exiting...\n");
+            return 1;
+        }
+
+        for (int j = 0; j < size; ++j) {
+            worst_arr[j] = size - j; // Assigning numbers in descending order
+        }
+
+        int worst_target = size + 1; // Target element that is not present in the array
+
+        long long worst_elapsed_time = measureElapsedTime(worst_arr, size, worst_target);
+        PROCESS_MEMORY_COUNTERS_EX worst_pmc;
+        GetProcessMemoryInfo(GetCurrentProcess(), (PROCESS_MEMORY_COUNTERS*)&worst_pmc, sizeof(worst_pmc));
+
+        insertion_sort(worst_arr, size); // Sort the array
+
+        GetProcessMemoryInfo(GetCurrentProcess(), (PROCESS_MEMORY_COUNTERS*)&worst_pmc, sizeof(worst_pmc));
+        SIZE_T worst_memory_usage = worst_pmc.PrivateUsage;
+
+        int worst_index = interpolation_search(worst_arr, size, worst_target);
+
+        if (worst_index != -1)
+            printf("Element found at index %d\n", worst_index);
+        else
+            printf("Element not found\n");
+
+        printf("Time elapsed: %lld nanoseconds\n", worst_elapsed_time);
+        printMemoryUsage(worst_pmc);
+
+        free(worst_arr);
+
+        printf("Worst Case Ended\n\n");
+
+        printf("// Case %d Completed\n\n", i + 1);
     }
-    printf("\n");
-
-    int target;
-    printf("Enter the target element: ");
-    scanf("%d", &target);
-
-    double elapsed_time = measureElapsedTime(arr, size, target);
-    SIZE_T peak_working_set_size = measureMemoryUsage(arr, size, target);
-
-    insertion_sort(arr, size); // Sort the array
-
-    printf("Sorted array: ");
-    for (int i = 0; i < size; ++i) {
-        printf("%d ", arr[i]);
-    }
-    printf("\n");
-
-    int index = interpolation_search(arr, size, target);
-
-    if (index != -1)
-        printf("Element found at index %d\n", index);
-    else
-        printf("Element not found\n");
-
-    printf("Time elapsed: %.6f seconds\n", elapsed_time);
-    printf("Peak working set size: %lu bytes\n", peak_working_set_size);
-
-    free(arr); // Free the dynamically allocated memory
 
     return 0;
 }
--- DSA/LinearSearchAlgorithm.c
+++ DSA/LinearSearchAlgorithm.c
@@ -4,16 +4,30 @@
 #include <psapi.h>
 #include <time.h>
 
-int linearSearch(int arr[], int n, int target) {
-    for (int i = 0; i < n; i++) {
-        if (arr[i] == target) {
-            return i;  // Return the index where the target element is found
+// Function to perform linear search in an array
+// Arguments:
+// - arr: The array to be searched
+// - n: The size of the array
+// - target: The element to be searched for
+// Returns:
+// - The index of the target element if found, or -1 if not found
+int linearSearch(int arr[], int n, int target)
+{
+    for (int i = 0; i < n; i++)
+    {
+        if (arr[i] == target)
+        {
+            return i; // Return the index where the target element is found
         }
     }
-    return -1;  // Return -1 if the target element is not found
+    return -1; // Return -1 if the target element is not found
 }
 
-long long getCurrentTime() {
+// Function to get the current time using the Windows performance counters
+// Returns:
+// - The current time in nanoseconds
+long long getCurrentTime()
+{
     LARGE_INTEGER frequency;
     QueryPerformanceFrequency(&frequency);
 
@@ -26,86 +40,123 @@ long long getCurrentTime() {
     return nanoseconds;
 }
 
-void printMemoryUsage() {
-    HANDLE process = GetCurrentProcess();
-    PROCESS_MEMORY_COUNTERS_EX pmc;
-    if (GetProcessMemoryInfo(process, (PROCESS_MEMORY_COUNTERS*)&pmc, sizeof(pmc))) {
-        SIZE_T virtualMemoryUsed = pmc.PrivateUsage;
-        printf("Memory Usage: %zu bytes\n", virtualMemoryUsed);
-    }
+// Function to print memory usage statistics
+// Arguments:
+// - pmc: The PROCESS_MEMORY_COUNTERS_EX structure containing memory usage information
+void printMemoryUsage(PROCESS_MEMORY_COUNTERS_EX pmc)
+{
+    SIZE_T virtualMemoryUsed = pmc.PrivateUsage;
+    SIZE_T peakWorkingSetSize = pmc.PeakWorkingSetSize;
+    printf("Memory Usage: %llu bytes\n", (unsigned long long)virtualMemoryUsed);
+    printf("Peak Working Set Size: %llu bytes\n", (unsigned long long)peakWorkingSetSize);
 }
 
-int main() {
-    int n = 5000; // Size of the array
-    int* arr = (int*)malloc(n * sizeof(int)); // Dynamically allocate memory for the array
+int main()
+{
+    int sizes[] = {5000, 10000, 15000}; // Array sizes for analysis
 
-    // Generate array elements
-    srand(time(NULL));
-    for (int i = 0; i < n; i++) {
-        arr[i] = rand() % 10000001; // Generating random numbers between 0 and 10,000,000
+    for (int i = 0; i < sizeof(sizes) / sizeof(sizes[0]); i++)
+    {
+        int n = sizes[i];
+        printf("Case %d (Size: %d)\n", i + 1, n);
+
+        // Allocate memory for the array
+        int *arr = (int *)malloc(n * sizeof(int));
+
+        // Generate array elements
+        srand(time(NULL));
+        for (int i = 0; i < n; i++)
+        {
+            arr[i] = rand() % 10000001; // Generating random numbers between 0 and 10,000,000
+        }
+
+        // Best Case
+        printf("Best Case Started\n");
+        // Perform linear search
+        long long start = getCurrentTime();
+        int target = arr[n - 1];
+        int result = linearSearch(arr, n, target);
+        long long end = getCurrentTime();
+        long long executionTime = end - start;
+        // Print result and execution time
+        if (result == -1)
+        {
+            printf("Element not found.\n");
+        }
+        else
+        {
+            printf("Element found at index %d.\n", result);
+        }
+        printf("Time elapsed: %lld nanoseconds\n", executionTime);
+        HANDLE process = GetCurrentProcess();
+        PROCESS_MEMORY_COUNTERS_EX pmc;
+        ZeroMemory(&pmc, sizeof(pmc));
+        pmc.cb = sizeof(pmc);
+        GetProcessMemoryInfo(process, (PROCESS_MEMORY_COUNTERS *)&pmc, sizeof(pmc));
+        printMemoryUsage(pmc);
+        printf("Best Case Ended\n\n");
+
+        // Reset variables for the next case
+        start = 0;
+        end = 0;
+        executionTime = 0;
+
+        // Average Case
+        printf("Average Case Started\n");
+        // Perform linear search
+        start = getCurrentTime();
+        target = arr[n / 2];
+        result = linearSearch(arr, n, target);
+        end = getCurrentTime();
+        executionTime = end - start;
+        // Print result and execution time
+        if (result == -1)
+        {
+            printf("Element not found.\n");
+        }
+        else
+        {
+            printf("Element found at index %d.\n", result);
+        }
+        printf("Time elapsed: %lld nanoseconds\n", executionTime);
+        ZeroMemory(&pmc, sizeof(pmc));
+        pmc.cb = sizeof(pmc);
+        GetProcessMemoryInfo(process, (PROCESS_MEMORY_COUNTERS *)&pmc, sizeof(pmc));
+        printMemoryUsage(pmc);
+        printf("Average Case Ended\n\n");
+
+        // Reset variables for the next case
+        start = 0;
+        end = 0;
+        executionTime = 0;
+
+        // Worst Case
+        printf("Worst Case Started\n");
+        // Perform linear search
+        start = getCurrentTime();
+        target = n + 1;
+        result = linearSearch(arr, n, target);
+        end = getCurrentTime();
+        executionTime = end - start;
+        // Print result and execution time
+        if (result == -1)
+        {
+            printf("Element not found.\n");
+        }
+        else
+        {
+            printf("Element found at index %d.\n", result);
+        }
+        printf("Time elapsed: %lld nanoseconds\n", executionTime);
+        ZeroMemory(&pmc, sizeof(pmc));
+        pmc.cb = sizeof(pmc);
+        GetProcessMemoryInfo(process, (PROCESS_MEMORY_COUNTERS *)&pmc, sizeof(pmc));
+        printMemoryUsage(pmc);
+        printf("Worst Case Ended\n\n");
+
+        free(arr); // Free dynamically allocated memory
+        printf("Case %d Completed\n\n", i + 1);
     }
-
-    // Best Case
-    printf("Best Case:\n");
-    int target = arr[0]; // Target element at the beginning of the array
-
-    long long start = getCurrentTime(); // Start the clock
-
-    int result = linearSearch(arr, n, target); // Perform linear search
-
-    long long end = getCurrentTime(); // Stop the clock
-
-    // Print the result and execution time
-    if (result == -1) {
-        printf("Element not found.\n");
-    } else {
-        printf("Element found at index %d.\n", result);
-    }
-    long long execution_time = end - start;
-    printf("Execution time: %lld nanoseconds.\n", execution_time);
-    printMemoryUsage();
-
-    // Average Case
-    printf("\nAverage Case:\n");
-    target = arr[n / 2]; // Target element at the middle of the array
-
-    start = getCurrentTime(); // Start the clock
-
-    result = linearSearch(arr, n, target); // Perform linear search
-
-    end = getCurrentTime(); // Stop the clock
-
-    // Print the result and execution time
-    if (result == -1) {
-        printf("Element not found.\n");
-    } else {
-        printf("Element found at index %d.\n", result);
-    }
-    execution_time = end - start;
-    printf("Execution time: %lld nanoseconds.\n", execution_time);
-    printMemoryUsage();
-
-    // Worst Case
-    printf("\nWorst Case:\n");
-    target = n + 1; // Target element not present in the array
-
-    start = getCurrentTime(); // Start the clock
-
-    result = linearSearch(arr, n, target); // Perform linear search
-
-    end = getCurrentTime(); // Stop the clock
-
-    // Print the result and execution time
-    if (result == -1) {
-        printf("Element not found.\n");
-    } else {
-        printf("Element found at index %d.\n", result);
-    }
-    execution_time = end - start;
-    printf("Execution time: %lld nanoseconds.\n", execution_time);
-    printMemoryUsage();
-
-    free(arr); // Free dynamically allocated memory
 
     return 0;
 }
