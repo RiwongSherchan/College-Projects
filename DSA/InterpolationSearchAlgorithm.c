8f87258cc7ea2f4a24b9b93bfddfd98e0a610b0c
RiwongSherchan <sherchanriwong@gmail.com>
2023-06-05 22:23:42 +0545

    Interpolation Search completed

--- DSA/InterpolationSearchAlgorithm.c
+++ DSA/InterpolationSearchAlgorithm.c
@@ -4,11 +4,13 @@
 #include <windows.h>
 #include <psapi.h>
 
+// Function to perform insertion sort on an array
 void insertion_sort(int arr[], int size) {
     for (int i = 1; i < size; ++i) {
         int key = arr[i];
         int j = i - 1;
 
+        // Move elements of arr[0..i-1], that are greater than key, to one position ahead of their current position
         while (j >= 0 && arr[j] > key) {
             arr[j + 1] = arr[j];
             --j;
@@ -18,30 +20,35 @@ void insertion_sort(int arr[], int size) {
     }
 }
 
+// Function to perform interpolation search on a sorted array
 int interpolation_search(int arr[], int size, int target) {
     int low = 0;
     int high = size - 1;
 
     while (low <= high && target >= arr[low] && target <= arr[high]) {
         if (low == high) {
+            // If the target element is found at the current position
             if (arr[low] == target)
                 return low;
-            return -1;
+            return -1; // Target element not found
         }
 
+        // Calculate the position using interpolation formula to narrow down the search range
         int position = low + (((double)(target - arr[low]) / (arr[high] - arr[low])) * (high - low));
 
+        // If the target element is found at the calculated position
         if (arr[position] == target)
             return position;
         else if (arr[position] < target)
-            low = position + 1;
+            low = position + 1; // Update the lower bound
         else
-            high = position - 1;
+            high = position - 1; // Update the upper bound
     }
 
-    return -1;
+    return -1; // Target element not found
 }
 
+// Function to measure the elapsed time of a function call
 long long measureElapsedTime(int arr[], int size, int target) {
     LARGE_INTEGER frequency, start, end;
     QueryPerformanceFrequency(&frequency);
@@ -49,7 +56,7 @@ long long measureElapsedTime(int arr[], int size, int target) {
 
     insertion_sort(arr, size); // Sort the array
 
-    interpolation_search(arr, size, target);
+    interpolation_search(arr, size, target); // Perform interpolation search
 
     QueryPerformanceCounter(&end);
     long long elapsed_time = (end.QuadPart - start.QuadPart) * 1000000000 / frequency.QuadPart;
@@ -57,6 +64,7 @@ long long measureElapsedTime(int arr[], int size, int target) {
     return elapsed_time;
 }
 
+// Function to print the memory usage information
 void printMemoryUsage(PROCESS_MEMORY_COUNTERS_EX pmc) {
     SIZE_T virtualMemoryUsed = pmc.PrivateUsage;
     SIZE_T peakWorkingSetSize = pmc.PeakWorkingSetSize;
@@ -64,12 +72,19 @@ void printMemoryUsage(PROCESS_MEMORY_COUNTERS_EX pmc) {
     printf("Peak Working Set Size: %llu bytes\n", (unsigned long long)peakWorkingSetSize);
 }
 
+// Function to print an array
+void printArray(int arr[], int size) {
+    for (int i = 0; i < size; ++i) {
+        printf("%d ", arr[i]);
+    }
+    printf("\n");
+}
 
 int main() {
     srand(time(NULL));  // Seed the random number generator
 
     // Define different array sizes for each case
-    int sizes[] = {5000, 10000, 15000};
+    int sizes[] = {10, 20, 30};
 
     for (int i = 0; i < sizeof(sizes) / sizeof(sizes[0]); ++i) {
         int size = sizes[i]; // Size of the array
@@ -97,10 +112,18 @@ int main() {
 
         insertion_sort(best_arr, size); // Sort the array
 
+        printf("Original Array:\n");
+        printArray(best_arr, size);
+
+        printf("Sorted Array:\n");
+        printArray(best_arr, size);
+
         GetProcessMemoryInfo(GetCurrentProcess(), (PROCESS_MEMORY_COUNTERS*)&best_pmc, sizeof(best_pmc));
         SIZE_T best_memory_usage = best_pmc.PrivateUsage;
 
         int best_index = interpolation_search(best_arr, size, best_target);
+
+        printf("Target Element: %d\n", best_target);
 
         if (best_index != -1)
             printf("Element found at index %d\n", best_index);
@@ -124,10 +147,10 @@ int main() {
         }
 
         for (int j = 0; j < size; ++j) {
-            average_arr[j] = rand() % 1001; // Generate random numbers between 0 and 1000
+            average_arr[j] = rand() % 81; // Generate random numbers between 0 and 80
         }
 
-        int average_target = rand() % 1001; // Random target element
+        int average_target = rand() % 81; // Random target element
 
         long long average_elapsed_time = measureElapsedTime(average_arr, size, average_target);
         PROCESS_MEMORY_COUNTERS_EX average_pmc;
@@ -135,10 +158,18 @@ int main() {
 
         insertion_sort(average_arr, size); // Sort the array
 
+        printf("Original Array:\n");
+        printArray(average_arr, size);
+
+        printf("Sorted Array:\n");
+        printArray(average_arr, size);
+
         GetProcessMemoryInfo(GetCurrentProcess(), (PROCESS_MEMORY_COUNTERS*)&average_pmc, sizeof(average_pmc));
         SIZE_T average_memory_usage = average_pmc.PrivateUsage;
 
         int average_index = interpolation_search(average_arr, size, average_target);
+
+        printf("Target Element: %d\n", average_target);
 
         if (average_index != -1)
             printf("Element found at index %d\n", average_index);
@@ -165,7 +196,7 @@ int main() {
             worst_arr[j] = size - j; // Assigning numbers in descending order
         }
 
-        int worst_target = size + 1; // Target element that is not present in the array
+        int worst_target = 1; // Target element is the first element
 
         long long worst_elapsed_time = measureElapsedTime(worst_arr, size, worst_target);
         PROCESS_MEMORY_COUNTERS_EX worst_pmc;
@@ -173,10 +204,18 @@ int main() {
 
         insertion_sort(worst_arr, size); // Sort the array
 
+        printf("Original Array:\n");
+        printArray(worst_arr, size);
+
+        printf("Sorted Array:\n");
+        printArray(worst_arr, size);
+
         GetProcessMemoryInfo(GetCurrentProcess(), (PROCESS_MEMORY_COUNTERS*)&worst_pmc, sizeof(worst_pmc));
         SIZE_T worst_memory_usage = worst_pmc.PrivateUsage;
 
         int worst_index = interpolation_search(worst_arr, size, worst_target);
+
+        printf("Target Element: %d\n", worst_target);
 
         if (worst_index != -1)
             printf("Element found at index %d\n", worst_index);
@@ -189,8 +228,6 @@ int main() {
         free(worst_arr);
 
         printf("Worst Case Ended\n\n");
-
-        printf("// Case %d Completed\n\n", i + 1);
     }
 
     return 0;
